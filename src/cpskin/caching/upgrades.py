bcc372d125806365b040f404fc95ff6dfb54ac6a
Benoit Suttor <bsuttor@imio.be>
2019-03-18 14:03:27 +0100

    fix upgrade step

--- src/cpskin/caching/upgrades.py
+++ src/cpskin/caching/upgrades.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+# from plone import api
 
 
 def reload_registry_xml(context):
+    # kid = 'plone.cachepurging.interfaces.ICachePurgingSettings.virtualHosting'
+    # api.portal.set_registry_record(kid, True)
     context.runImportStepFromProfile(
-        'profile-cpskin.workflow:default', 'plone.app.registry')
+        'profile-cpskin.caching:default', 'plone.app.registry')
