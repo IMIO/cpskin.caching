07bd7190a1147372abc7dfc1253e1d98e8586963
Benoit Suttor <bsuttor@imio.be>
2019-03-18 14:04:36 +0100

    Back to development: 1.1.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ long_description = (
 
 setup(
     name="cpskin.caching",
-    version="1.1.1",
+    version="1.1.2.dev0",
     description="Caching configuration for cpskin",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
